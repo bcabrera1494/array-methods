daed83827e73fea61be398f001a3b0532e54052f
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-26 22:32:25 -0700

    Create object toCamelize and add values

---
+++ 01-camelize-str/script.js
@@ -0,0 +1,11 @@
+let toCamelize = {
+    1: 'jeremy-allen-white',
+    2: 'timothee-chalemet',
+    3: 'james-earl-jones',
+    4: 'kopi-joe-widdle',
+    5: 'mary-tyler-moore',
+    6: ''
+}
+function camelize(str){
+    str.split('-')
+}
