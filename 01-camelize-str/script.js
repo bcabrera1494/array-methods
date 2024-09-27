a0005f6b00092b291299b965279af3f9b0e57eac
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-27 13:11:25 -0700

    Use index, .toUpperCase, and slice to write function to capitalize the first letter of a string.

--- 01-camelize-str/script.js
+++ 01-camelize-str/script.js
@@ -1,14 +1,22 @@
-// let toCamelize = ['jeremy-allen-white', 'olivia-newton-john', 'kopi-joe-widdle', 'mary-tyler-moore', 'james-earl-jones', 'mary-j-blige', 'sterling-k-brown', 'catherin-zeta-jones'];
 let string = 'kopi-joe-widdle';
 
 function camelize(str){
+    // split the string by '-' delim
     let split = str.split('-');
-    split;
+    return split;
+    // Extract the 1st letter of each array item
 };
 
-function upperCase(ltr){
-    //get a single character string "ltr"
+function upperCase(ltr, index){
+    //get a single character string "ltr" - use index 0?
+    let modLtr = ltr[index].toUpperCase() + ltr.slice(1);
+    return modLtr;
     //call the ltr.toUpperCase();
-}
+};
+
+function concatStr(str){
+    //rejoin the capital letters to the string
+};
 
 console.log(camelize(string));
+console.log(upperCase(string, 0));
