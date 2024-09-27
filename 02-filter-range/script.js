d4b321d6933c7e5365eeed8c66668f897680bac7
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-27 14:20:25 -0700

    Initialize arr. Write pseudocode for function.

--- 02-filter-range/script.js
+++ 02-filter-range/script.js
@@ -1,3 +1,11 @@
-filterRange (arr, a, b){
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function filterValues (arr) {
     
 }
+
+filterRange (arr, a, b){
+    //Filter method
+    arr.filter()
+    //Function that returns values that are a<= and b>=
+};
