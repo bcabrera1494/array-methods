a89f88397beb4f4a3d41b07505a2dc410035f613
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-27 12:57:01 -0700

    Create function to split strings by - delim. console.log to test that it works as expected.

--- 01-camelize-str/script.js
+++ 01-camelize-str/script.js
@@ -1,16 +1,7 @@
-let toCamelize = {
-    1: 'jeremy-allen-white',
-    2: 'olivia-newton-john',
-    3: 'james-earl-jones',
-    4: 'kopi-joe-widdle',
-    5: 'mary-tyler-moore',
-    6: 'mary-j-blige',
-    7: 'catherine-zeta-jones',
-};
+// let toCamelize = ['jeremy-allen-white', 'olivia-newton-john', 'kopi-joe-widdle', 'mary-tyler-moore', 'james-earl-jones', 'mary-j-blige', 'sterling-k-brown', 'catherin-zeta-jones'];
+let string = 'kopi-joe-widdle';
+
 function camelize(str){
     let split = str.split('-');
     return split;
 }
-
-console.log(toCamelize);
-console.log(toCamelize.isArray);
