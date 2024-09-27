5751cac2432d14001f28c209af86dcdecca8d66f
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-26 22:41:08 -0700

    Write logic to split strings using '-' delim

--- 01-camelize-str/script.js
+++ 01-camelize-str/script.js
@@ -1,11 +1,16 @@
 let toCamelize = {
     1: 'jeremy-allen-white',
-    2: 'timothee-chalemet',
+    2: 'olivia-newton-john',
     3: 'james-earl-jones',
     4: 'kopi-joe-widdle',
     5: 'mary-tyler-moore',
-    6: ''
-}
+    6: 'mary-j-blige',
+    7: 'catherine-zeta-jones',
+};
 function camelize(str){
-    str.split('-')
+    let split = str.split('-');
+    return split;
 }
+
+console.log(toCamelize);
+console.log(toCamelize.isArray);
