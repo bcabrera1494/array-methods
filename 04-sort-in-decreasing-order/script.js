eabb12976746e15a78837de42867364c6a9f7426
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-27 15:18:28 -0700

    Reverse the logic under compareNumbers. This now sorts the array in descending order.

--- 04-sort-in-decreasing-order/script.js
+++ 04-sort-in-decreasing-order/script.js
@@ -1,9 +1,9 @@
 let array = [14, 3, 2, 33];
 
 function compareNumbers(a, b){
-if (a > b) return 1;
+if (a < b) return 1;
 if (a == b) return 0;
-if (a < b) return -1;
+if (a > b) return -1;
 };
 
 function sortDecreasing (arr){
