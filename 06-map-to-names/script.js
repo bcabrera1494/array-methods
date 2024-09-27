5c57be3a3f4541b3ff00d5babbf064688c18703e
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-27 15:41:03 -0700

    Initialize users and each user as objects with name and age.

--- 06-map-to-names/script.js
+++ 06-map-to-names/script.js
@@ -1,10 +1,13 @@
 // Create object with names and ages
-// let john = { name: "John", age: 25 };
-// let pete = { name: "Pete", age: 30 };
-// let mary = { name: "Mary", age: 28 };
 
 // let users = [ john, pete, mary ];
 
 // let names = /* ... your code */
 
 // alert( names ); // John, Pete, Mary
+
+let kopi = { name: "Kopi", age: 5};
+let mama = { name: "Sydney", age: 28};
+let beau = { name: "Beau", age: 29};
+
+let users = [kopi, mama, beau];
