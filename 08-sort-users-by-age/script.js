8fc59897327fe38c7b1fe7db32f4c163bbabad8d
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-30 16:46:59 -0700

    Rework the array.sort method logic for comparison to be directly to the array object properties using keys. Exercise complete.

--- 08-sort-users-by-age/script.js
+++ 08-sort-users-by-age/script.js
@@ -1,16 +1,16 @@
-let kopi = {name: "Kopi", age: "5"};
-let mama = {name: "Sydney", age: "28"};
-let papa = {name: "Bea", age: "29"};
+let kopi = { name: "Kopi", age: "5" };
+let mama = { name: "Sydney", age: "28" };
+let papa = { name: "Bea", age: "29" };
 
 let users = [papa, kopi, mama];
 
 function sortByAge(array) {
-    let ages = array.map((user) => user.age);
-    ages.sort((a, b) =>  a - b);
-    return array;
+
+    array.sort((a, b) =>  a.age - b.age)
 };
 
-console.log(users[0]);
-console.log(users[1]);
-console.log(users[2]);
-console.log(sortByAge(users));
+sortByAge(users);
+console.log(users);
+console.log(users[0]); //should be kopi
+console.log(users[1]); //should be mama
+console.log(users[2]); //should be papa
