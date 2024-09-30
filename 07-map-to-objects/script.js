fdfd0344251a666fe8ac3dc72a27c640491e9f96
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-30 15:40:47 -0700

    Rework code so that userMapped creates a new object using information accessed from the original user objects. Console log to test. Exercise complete.

--- 07-map-to-objects/script.js
+++ 07-map-to-objects/script.js
@@ -4,6 +4,9 @@ let beau = {name: "Bea", surname: "Cabrera", id: 3};
 
 let users = [kopi, mama, beau];
 
-let userMapped = users.map(item => `${item.name} ${item.surname}`);
+let userMapped = users.map(item => ({fullName: `${item.name} ${item.surname}`, id: item.id }));
 
 console.log(userMapped);
+console.log(userMapped[0].fullName);
+console.log(userMapped[1].fullName);
+console.log(userMapped[2].fullName);
