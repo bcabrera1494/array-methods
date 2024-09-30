178fc760dc2c62b9df6efa520c673ffe3d41d8f4
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-30 14:04:35 -0700

    Declare objects, declare users, declare names. Console.log to test referenes to users and names.

--- 06-map-to-names/script.js
+++ 06-map-to-names/script.js
@@ -1,6 +1,5 @@
 // Create object with names and ages
 
-// let users = [ john, pete, mary ];
 
 // let names = /* ... your code */
 
@@ -11,3 +10,9 @@ let mama = { name: "Sydney", age: 28};
 let beau = { name: "Beau", age: 29};
 
 let users = [kopi, mama, beau];
+
+console.log(users);
+
+let names = [kopi.name, mama.name, beau.name];
+
+console.log(names);
