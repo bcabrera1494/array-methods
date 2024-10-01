ff55d9d6d46048a14d1c7b06989717aab13ed972
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-01 14:45:07 -0700

    Write pseudocode for function.

--- 11-filter-unique-array-members/script.js
+++ 11-filter-unique-array-members/script.js
@@ -1,7 +1,15 @@
 let strings = ["kopi", "joe", "kopi", "joe", "widdle", "kopi", "joe", "nub"];
 
-function unique(arr){
+function unique(arr) {
     // code to filter unique array members
-    // return unique members
+    // initialize a new arary
+    let uniqueStrings = new Array;
+    // map through the array with a filter function
+    // conditional statement: 
+    // if item is not present in the new array, add item to the new array
+    // if item is present in the new array, continue
+    // return unique members in new array
     // return should be kopi, joe, widdle, nub
-}
+};
+
+console.log(unique(strings));
