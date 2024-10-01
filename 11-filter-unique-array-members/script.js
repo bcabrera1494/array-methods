caea48208096f51a95802059216248d32e09ffc1
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-01 14:28:06 -0700

    First commit exercise 11.

---
+++ 11-filter-unique-array-members/script.js
@@ -0,0 +1,7 @@
+let strings = ["kopi", "joe", "kopi", "joe", "widdle", "kopi", "joe", "nub"];
+
+function unique(arr){
+    // code to filter unique array members
+    // return unique members
+    // return should be kopi, joe, widdle, nub
+}
