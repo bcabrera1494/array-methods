56ab1d912df0b1aa39d4bad2b454062d09805c6f
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-01 15:45:35 -0700

    Use map and if statement to filter unique string items from the original string. Method checks if the uniqueStrings array has the item and, if not, adds the item. If it already includes the item, it does not add the item to the uniqueStrings array. Works as expected. Exercise 11 complete.

--- 11-filter-unique-array-members/script.js
+++ 11-filter-unique-array-members/script.js
@@ -1,15 +1,13 @@
 let strings = ["kopi", "joe", "kopi", "joe", "widdle", "kopi", "joe", "nub"];
 
 function unique(arr) {
-    // code to filter unique array members
     // initialize a new arary
     let uniqueStrings = new Array;
-    // map through the array with a filter function
-    // conditional statement: 
-    // if item is not present in the new array, add item to the new array
-    // if item is present in the new array, continue
-    // return unique members in new array
-    // return should be kopi, joe, widdle, nub
+    // Filter array -> filter function: check each item to see if included in uniqueStrings
+    strings.map(item => { if (!uniqueStrings.includes(item) === true) uniqueStrings.push(item) });
+    return uniqueStrings;
 };
+// if item is not present in the uniqueStrings, add item to the uniqueStrings
+// return should be kopi, joe, widdle, nub
 
 console.log(unique(strings));
