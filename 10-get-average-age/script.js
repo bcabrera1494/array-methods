dc16f5c687aed0c8eabb8496cbcef964b0d917f9
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-01 14:20:11 -0700

    Write user objects, array of ages, array of users, and getAverageAge function using reduce method. Console.log to test.

---
+++ 10-get-average-age/script.js
@@ -0,0 +1,18 @@
+let kopi = { name: "Kopi", age: 5 };
+let mama = { name: "Sydney", age: 28 };
+let papa = { name: "Bea", age: 29 };
+
+// access the users ages
+let ages = [kopi.age, mama.age, papa.age];
+let users = [kopi, mama, papa];
+
+function getAverageAge(array) {
+    // add the users ages
+    let arraySum = array.reduce((sum, current) => sum + current, 0);
+    // divide by number of ages to calculate average
+    let averageAge = arraySum / 4;
+    return averageAge
+    // return average age
+}
+
+console.log(ages);
