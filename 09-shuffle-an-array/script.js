59c717d1a69e6b1ec82fc15dbea2333d3e7b0fa3
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-01 14:08:34 -0700

    Write another version of the shuffle function using fisher-yates method. Method is still a bit confusing to understand.

--- 09-shuffle-an-array/script.js
+++ 09-shuffle-an-array/script.js
@@ -6,3 +6,17 @@ function shuffleArray(arr){
 };
 
 console.log(shuffleArray(array));
+
+//Fisher-Yates Shuffle
+
+function fisherShuffle(arr){
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [arr[i]], arr[j] = [arr[j], arr[i]];
+    }
+    return arr;
+};
+
+let arrayTwo = [5, 4, 3, 2, 1];
+
+console.log(fisherShuffle(arrayTwo));
