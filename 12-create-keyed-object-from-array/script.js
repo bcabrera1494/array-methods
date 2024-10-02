4e9ccd025d155cb1c6da349f3607f52c8451c32a
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-01 17:33:47 -0700

    Manually change key for objects in obj to match their corresponding id from users. Currently returns an array with the original unlabeled keys (0, 1, 2) with duplicates that are keyed with the id names. Need to use .reduce method to remove the unkeyed objects from obj.

--- 12-create-keyed-object-from-array/script.js
+++ 12-create-keyed-object-from-array/script.js
@@ -1,24 +1,24 @@
 let users = [
-    {id: 'kopi', name: 'Kopi Joe Widdle', age: 5},
-    {id: 'mama', name: 'Sydney Kate Sullivan', age: 28},
-    {id: 'papa', name: 'Beau Joe Widdle', age: 29},
+    { id: 'kopi', name: 'Kopi Joe Widdle', age: 5 },
+    { id: 'mama', name: 'Sydney Kate Sullivan', age: 28 },
+    { id: 'papa', name: 'Beau Joe Widdle', age: 29 },
 ];
 
-function groupById(array){
-// declare an empty object
-let obj = new Object(array);
+function groupById(array) {
+    // declare an empty object
+    let obj = new Object(array);
 
-obj[array[0].id] = obj[0];
-// add the id as the group label + the full object to obj
+    // add the id as the group label + the full object to obj *** Done individually, but there may be an more elegant way
+        // for (let i = 0; i < array.length; i++) {
+        //     array.map(i => obj[array[i].id] = obj[i])
+        // };
+    obj[users[0].id] = obj[0];
+    obj[users[1].id] = obj[1];
+    obj[users[2].id] = obj[2];
 
-// Object.defineProperty(obj[0], 'id', (users[0].id)); ***Don't understand how to use .defineProperty***
-
-// array.reduce(() => {});
-// //return the newly created object
-return obj;
-}; 
+    // array.reduce(() => {});
+    // //return the newly created object
+    return obj;
+};
 
 console.log(groupById(users));
-console.log(users[0]);
-console.log(users[1]);
-console.log(users[2]);
